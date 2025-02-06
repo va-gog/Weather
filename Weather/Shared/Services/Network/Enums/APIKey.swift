c8fa786223c24d1a2fa2f33f536784cacff27ee4
Gohar <abg_abg@bk.ru>
2025-02-06 10:22:57 +0400

    Removed files

--- Weather/Shared/Services/Network/Enums/APIKey.swift
+++ Weather/Shared/Services/Network/Enums/APIKey.swift
@@ -8,5 +8,5 @@
 /*fetch this securely, possibly from environment variables or secure storage
  This could be configured per environment in a CI/CD setup */
 enum APIKey: String {
-    case weatherAPI = "c44d8a4d7dbc0e470912963c69287a76"
+    case weatherAPI = ""
 }
